205d61602ce8fb0915862b7d681d27367a2c5132
olga <bend_sinister@mail.ru>
2022-10-12 21:31:23 +0300

    T50

---
+++ Task50/Program.cs
@@ -0,0 +1,69 @@
+﻿// Напишите программу, которая на вход принимает позицию элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+//17 -> такого числа в массиве нет
+//5 -> 9
+//2 -> 7
+//9 -> 4
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.WriteLine();
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] twoDArray = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+print2DArray(twoDArray);
+Console.WriteLine();
+Console.Write("Введите целое число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if(userNumber > 11)
+{
+    Console.Write($"Элемент с номером {userNumber} не существует "); 
+}
+
+for (int i = 0; i < twoDArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < twoDArray.GetLength(1); j++)
+        {
+            int k = i * 4 + j;
+            if(userNumber == k)
+            {
+              Console.Write($"Элемент с номером {userNumber} : {twoDArray[i, j]} ");  
+            }
+        }
+       
+    }
+
+
